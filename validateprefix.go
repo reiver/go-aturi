4126a0f457ba0dd19ba97ae736ea6798deb1f1b9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-04-30 07:03:18 -0700

    docs

--- validateprefix.go
+++ validateprefix.go
@@ -4,16 +4,19 @@ import (
 	"github.com/reiver/go-erorr"
 )
 
-// ValidatePrefix only validates the prefix (i.e., "at://") of an AT-URI.
+// ValidatePrefix only validates the prefix (i.e., "at://") of a potential AT-URI.
 //
 // So, it checks to see if the URI starts with "at://".
 // And, that is it.
 //
-// You would use ValidatePrefix if you wanted to be very liberal in what you accept as a valid AT-URI.
+// You would use ValidatePrefix if you wanted to be very liberal in what you accept as a valid AT-URI, including accepting an empty 'authority'.
+// I.e., this does a bit more than [ValidatePrefix].
 //
-// ValidatePrefix calls [ValidateScheme] internally.
+// ValidatePrefix calls [ValidatePrefix] internally.
 //
 // For a more thorough validation of the whole AT-URI instead use [Validate].
+//
+// Alternatively, to validate a bit more than ValidatePrefix, without being as thorough as [Validate], instead use [ValidateGenerically].
 func ValidatePrefix(uri string) error {
 
 	if err := ValidateScheme(uri); nil != err {
