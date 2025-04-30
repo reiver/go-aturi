48b39680045bd4e1070876e8b9776c44bb1218ac
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-04-30 06:45:02 -0700

    docs

--- validatescheme.go
+++ validatescheme.go
@@ -17,7 +17,11 @@ const lenSchemePrefix int = len(schemePrefix)
 // You would use ValidateScheme if you wanted to be very liberal in what you accept as a valid AT-URI, including not caring if the AT-URI has an 'authority' or not.
 // I.e., this is the minimum amount of validation you can do to validate an AT-URI.
 //
-// For more thorough validation of the whole AT-URI instead use [Validate].
+// Note that you are passing ValidateScheme the whole URI, and not just the scheme.
+//
+// For a more thorough validation of the whole AT-URI instead use [Validate].
+//
+// Alternatively, to validate a bit more than ValidateScheme, without being as thorough as [Validate], instead use [ValidatePrefix].
 func ValidateScheme(uri string) error {
 
 	if "" == uri {
